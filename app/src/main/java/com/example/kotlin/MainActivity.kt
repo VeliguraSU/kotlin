e93c0282128a6a41b625fc3caed37c9acad1cf3b
android <https://github.com/Tortoewka/android.git>
2021-12-12 16:35:08 +0500

    lesson 1 я создал котлин проект, дата класс вывел из дата-класса параметры во вью.

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val tv: TextView? = null
+    private val buttontv: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val person = Person("Ivan", "Ivanov", 29)
         setContentView(R.layout.activity_main)
+        val tv = findViewById<TextView>(R.id.tv)
+        val tv2 = findViewById<TextView>(R.id.tv2)
+        val buttontv = findViewById<Button>(R.id.button_tv)
+        buttontv.setOnClickListener { view: View? ->
+            tv.text = person.Name
+            tv2.text = person.Name2
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/kotlin/Person.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin
+
+
+data class Person(var Name: String= "Ivan", var Name2: String="Vianov", var age: Int=30)
